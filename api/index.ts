9639ee046bf586d3107f59c08d6a2a0385f0608e
Adithya Pai B <paiadithya26@gmail.com>
2021-05-05 17:42:04 +0530

    Hindi language words not reporting fixed

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,6 @@ function hello(req, res) {
     res.statusCode = 200;
     res.json({ status: 'online',
         intsructions: "send the sentence with _ (replace spaces with _ )format to the /api end point",
-        eg: "api/chal_nikal_loveday" });
+        eg: "api/cusswords" });
 }
 exports["default"] = hello;
