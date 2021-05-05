768b3c31f7613f14590db1b2b2f3acc0b6d0cc88
Adithya Pai B <paiadithya26@gmail.com>
2021-05-05 10:00:05 +0530

    updated end point🔥

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,8 @@ exports.__esModule = true;
 function hello(req, res) {
     res.statusCode = 200;
     res.json({ status: 'online',
+        endpoint: '/api',
         intsructions: "send the sentence with _ (replace spaces with _ )format to the /api end point",
-        eg: "api/chal_nikal_loveday" });
+        eg: "api/strings_cussword" });
 }
 exports["default"] = hello;
