db42200b9eaef6ca1f28b3cb63dec425ef665a42
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2021-10-01 20:39:44 +0545

    Update gali.ts
    added nepali gaali

--- api/gali.ts
+++ api/gali.ts
@@ -12,5 +12,7 @@ let marathiGali: string[] = marathi.map(name => name.toLowerCase());
 const hindiGaali: string[] = ['behenkelode','chutmarike','kutreya','lawdeya','randiputram','tattokesaudagar','burkeburade','lundkedhakkhan','lukke','gandfatikebhadwe','bsdiwale','gaaandmarale']
 const bengali: string[] = ["madarchod", "chudi", "bokachoda", "khankirchele", "khanki", "gudmarani", "podmarani", "rendirchele", "bichirbal", "banchod", "bnchod", "magi", "kuttachoda", "hatichoda", "dianorsorchoda", "teponmagi", "rendimagi", "paglachoda", "bal", "besshamagi", "homochoda", "bara", "garmarani", "satavhanga", "vhangavhagi", "kalanichoda", "chutmarani", "barachoda", "garpaka", "khankimargir gud", "chodonmagi", "chodon64", "chudir vhai", "bal"]
 let bengaliGali: string[] = bengali.map(name => name.toLowerCase());
-const gali: string[] = [...hindiGali, ...tuluGali, ...hindiG, ...englishGali, ...melayuGali, ...marathiGali, ...hindiGaali, ...bengaliGali];
+const nepali: string[] = ["muji","raadi","randi","randi ko ban" ,"radikoban","bhalu","besya","chik"<"chiknu","puti","lado","randikochoro","randi ko choro","machikne","maachikne"]
+let nepaliGali: string[] = nepali.map(name => name.toLowerCase());
+const gali: string[] = [...hindiGali, ...tuluGali, ...hindiG, ...englishGali, ...melayuGali, ...marathiGali, ...hindiGaali, ...bengaliGali,...nepaliGali];
 module.exports = gali;
