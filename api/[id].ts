df57063c5bd92b4400f37f79c494f9136199c6e9
Luiz Henrique Alves Pansarini <luiz.pansarini@luizalabs.com>
2022-10-15 09:59:33 -0300

    fix: change imports and logic

--- api/[id].ts
+++ api/[id].ts
@@ -1,15 +1,15 @@
 var gali = require('./gali');
-export default function fetchUser(req, res) {
-    res.statusCode = 200;
-    let {id }= req.query;
-    id=id.toLowerCase();
-    id = id.split('_').join(' ');
-    const exists = id.split(' ').some(str => gali.includes(str))
-    if(exists)
-    res.json("true");
-    else
-    {
-      res.json("false");
-    }
 
+export default function fetchUser(req, res) {
+  const { id } = req.query;
+
+  const preparedId = id.replace(/_/g, ' ');
+
+  const exists = preparedId.split(' ').some(str => gali.includes(str))
+
+  if (exists) {
+    return res.status(200).json("true");
   }
+
+  return res.status(200).json("false")
+}
