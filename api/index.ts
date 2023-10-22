0e716e12d71af8a61ecba9db2591e10f32f102fd
Adithya Pai B <paiadithya26@gmail.com>
2023-10-22 17:44:00 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
-"use strict";
-exports.__esModule = true;
-function hello(req, res) {
-    res.statusCode = 200;
-    res.json({ status: 'online',
-        intsructions: "send the sentence with _ (replace spaces with _ )format to the /api end point",
-        eg: "api/cusswords" });
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function hello(req: VercelRequest, res: VercelResponse) {
+  res.status(200).send({
+    status: 'online',
+    instructions: "send the sentence with _ (replace spaces with _ ) format to the /api endpoint",
+    eg: "api/cusswords"
+  });
 }
-exports["default"] = hello;
